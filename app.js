584b1d8ffb54a82e6552c2bdded7e1c40af7b2c9
Puru Sewda <puru.sewda@mtxb2b.com>
2021-06-07 00:50:29 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+new hoverEffect ({
+    parent: document.querySelector('.ticket'),
+    intensity1: 0.1,
+    intensity2: 0.2,
+    angle2: Math.PI / 2,
+    speedIn: 6,
+    speedOut: 4,
+    image1: 'https://images.unsplash.com/photo-1619991015432-fd8c2bd454cf?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIwMDc5NzQ2&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600',
+    image2: 'https://images.unsplash.com/photo-1620010503383-78a77fa3a1ab?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIwMDc5ODkx&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600',
+    displacementImage: 'https://raw.githubusercontent.com/robin-dela/hover-effect/b6c6fd26/images/stripe1mul.png?raw=true%27'
+})
